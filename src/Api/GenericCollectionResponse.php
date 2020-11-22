666a391e04047eb6812d268623eefa4c9ad98dca
David TOUREL <contact@dynamicnet.fr>
2020-11-22 19:38:46 +0100

    Response lazy JSON decode

--- src/Api/GenericCollectionResponse.php
+++ src/Api/GenericCollectionResponse.php
@@ -8,6 +8,7 @@ class GenericCollectionResponse extends GenericResponse
 {
     public function getTotal()
     {
+        $this->decode_json();
         return $this->json_data->data->total??0;
     }
 }
--- src/Api/GenericResponse.php
+++ src/Api/GenericResponse.php
@@ -18,46 +18,62 @@ class GenericResponse
 
     protected $json_data;
 
+    protected $json_is_decoded = false;
+
     public function __construct(ResponseInterface $response)
     {
         $this->response = $response;
+    }
 
-        $contentType = $response->getHeaderLine('Content-Type');
+    protected function decode_json()
+    {
+        $contentType = $this->response->getHeaderLine('Content-Type');
 
         if (0 !== \strpos($contentType, 'application/json')) {
             throw new ResponseException('Cannot instanciate response with Content-Type: '.$contentType);
         }
 
-        $body = $response->getBody()->__toString();
+        $body = $this->response->getBody()->__toString();
         $this->json_data = json_decode($body);
 
         if (JSON_ERROR_NONE !== \json_last_error()) {
             throw new ResponseException(sprintf('Error (%d) when trying to json_decode response', \json_last_error()));
         }
+
+        $this->json_is_decoded = true;
     }
 
     public function success()
     {
+        $this->decode_json();
         return (floor($this->response->getStatusCode() / 100) == 2);
     }
 
     public function getErrorCode()
     {
+        $this->decode_json();
         return $this->json_data->code;
     }
 
     public function getErrorMsg()
     {
+        $this->decode_json();
         return \trim(\explode(":", $this->json_data->message, 2)[1]);
     }
 
     public function getRawData()
     {
+        $this->decode_json();
         return $this->json_data;
+    }
+
+    public function getRaw() {
+        return $this->response->getBody()->__toString();
     }
 
     public function getData()
     {
+        $this->decode_json();
         return $this->json_data->data->data??$this->json_data->data??[];
     }
 }
